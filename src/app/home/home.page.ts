d59116ff56ba2d2c964b0a090e88eb3d70709e8f
André Lopes <yonolino@live.com>
2022-02-26 15:35:28 -0300

    Commit 57

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef } from '@angular/core';
 import { ActionSheetController, AlertController, ToastController } from '@ionic/angular';
 import { Button } from 'protractor';
 import { stringify } from 'querystring';
@@ -12,10 +12,10 @@ export class HomePage {
 
   tasks: any[] = [];
 
-  constructor(private alertCtrl: AlertController, private toastCrtl: ToastController, private actionSheetCrtl: ActionSheetController) { 
+  constructor(private alertCtrl: AlertController, private toastCrtl: ToastController, private actionSheetCrtl: ActionSheetController) {
     let tasksJSON = localStorage.getItem('tasksDb');
 
-    if(tasksJSON!=null){
+    if (tasksJSON != null) {
       this.tasks = JSON.parse(tasksJSON);
     }
 
@@ -57,42 +57,72 @@ export class HomePage {
   async add(taskToDo: string) {
     if (taskToDo.trim().length < 1) {
       const toast = await this.toastCrtl.create({
-        message : 'Informe o que deseja fazer!',
-        duration : 2000,
-        position : 'middle',
-        color : 'danger',
+        message: 'Informe o que deseja fazer!',
+        duration: 2000,
+        position: 'middle',
+        color: 'danger',
       });
       toast.present();
+      return;
     }
-    let task = {name : taskToDo, done: false}
+    for(let i=0;i<this.tasks.length;i++){
+      if(taskToDo.trim()==this.tasks[i].name){
+        const toast = await this.toastCrtl.create({
+          message: 'Essa tarefa já está registrada!',
+          duration: 2000,
+          position: 'middle',
+          color: 'danger',
+        });
+        toast.present();
+        return;
+      }
+    }
+
+    let task = { name: taskToDo.trim(), done: false }
     this.tasks.push(task);
 
     this.updateLocalStorage();
   }
 
-  updateLocalStorage(){
+  updateLocalStorage() {
     localStorage.setItem('tasksDb', JSON.stringify(this.tasks));
   }
 
-  async openActions(task : any){
+  async openActions(task: any) {
     const actionSheet = await this.actionSheetCrtl.create({
       header: 'O QUE DESEJA FAZER?',
       buttons: [{
-        text : task.done ? 'Desmarcar' : 'Marcar',
-        icon : task.done ? 'radio-button-off' : 'checkmark-circle',
+        text: task.done ? 'Desmarcar' : 'Marcar',
+        icon: task.done ? 'radio-button-off' : 'checkmark-circle',
         handler: () => {
           task.done = !task.done;
           this.updateLocalStorage();
         }
-      },{
+      }, {
         text: 'Cancelar',
         icon: 'close',
         role: 'cancel',
         handler: () => {
-          console.log('Cancel clicked.')
+          console.log('Cancel clicked.');
         }
       }]
     });
     await actionSheet.present();
+  }
+
+  removeTask(task: any) {
+    const elementList = document.querySelectorAll('.listItem');
+    let element : Element;
+    for(let i=0;i<elementList.length;i++){
+      if(elementList[i].textContent==task.name+'Excluir'){
+        element=elementList[i];
+      }
+    }
+    element.classList.add('animate__animated', 'animate__bounceOutLeft');
+    console.log(element);
+    element.addEventListener('animationend', () => {
+      this.tasks = this.tasks.filter(taskArray => task != taskArray);
+      this.updateLocalStorage();
+    });
   }
 }
